4308803cfa6c5d37d8ee4de77606c7d45ca183b1
luckyxgaiss <90754094+luckyxgaiss@users.noreply.github.com>
2021-10-28 23:41:09 +0800

    Add files via upload

---
+++ 反向输出.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define N 10
+int main(){
+	int a[N],i;
+	printf("enter %d number:",N);
+	for(i=0;i<N;i++)
+	  scanf("%d",&a[i]);
+	printf("In reverse order:");
+	for (i=N-1;i>=0;i--)
+	   printf("%d",a[i]);
+	   printf("\n");
+	   return 0;
+}
