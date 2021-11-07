bb9de80b34df81a4a581b4d532c32268cf177f60
luckyxgaiss <90754094+luckyxgaiss@users.noreply.github.com>
2021-11-07 23:29:24 +0800

    Add files via upload

---
+++ 勾股定理.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int judge(int x,int y,int z){
+    if(x*x+y*y==z*z||x*x+z*z==y*y||z*z+y*y==x*x) return 1;
+    return 0;
+}
+int main(){
+	int i,n,a[n],b[n],c[n];
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d %d %d",&a[i],&b[i],&c[i]);
+	}
+	for(i=0;i<n;i++){
+		if(judge(a[i],b[i],c[i])) printf("Yes\n");
+		else printf("No\n");
+	}
+	return 0;
+}
