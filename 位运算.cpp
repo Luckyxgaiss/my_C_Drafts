dbafa3037626cd9f5f29d3ab4c12cccd854ff376
luckyxgaiss <90754094+luckyxgaiss@users.noreply.github.com>
2021-12-09 22:57:56 +0800

    Add files via upload

---
+++ 位运算.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	printf("%d\n",~1011);//取反 
+	printf("%d\n",0X0F);//十六进制 
+	printf("%d\n",1&0);//与运算 
+	printf("%d\n",1&1);//与运算 
+	printf("%d\n",1^0);//异或运算 
+	printf("%d\n",1|0);//或运算
+	return 0; 
+}
