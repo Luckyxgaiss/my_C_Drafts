a24fa03c915530af754885ff7e9a2828ad01d02c
luckyxgaiss <90754094+luckyxgaiss@users.noreply.github.com>
2021-12-18 23:24:36 +0800

    Add files via upload

--- superprime2.cpp
+++ superprime2.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
-int prime(int x){
+#include<math.h>
+int isprime(int x){
 	int i,l=0;
 	if(x<=1) return 0;
 	if(x==2) return 1;
@@ -13,17 +14,30 @@ int prime(int x){
 int main(){
 	int n;
 	scanf("%d",&n);
-	if(prime(n)) printf("yes\n");
+	if(isprime(n)) printf("yes\n");
 	else printf("no\n");
-	int num[10],len=0,sum1=0,sum2=0,i,k;
-    		for(k=0;;k++){
-    			num[k]=n%10;
-    			len++;
-    			n/=10;
-    			if(n==0) break;
+	int num[10],len=0,sum1=0,sum2=0,i,k,l=0,p=0,q=0;
+    for(k=0;;k++){
+    		num[k]=n%10;
+    		len++;
+    		n/=10;
+    		if(n==0) break;
 			}
-	for(i=0;i<len;i++){
-		printf("%d\n",num[i]);
+	for(i=len-1;i>=0;i--){
+		if(isprime(num[i])==0)
+		l++;
+	}
+	for(i=len-1;i>=0;i--){
+	    sum1+=num[i];
+	}
+	if(isprime(sum1)==0)  p++;
+	for(i=len-1;i>=0;i--){
+		if(isprime(num[i]*num[i])==0){
+			q++;
+		}
+	}
+	if(l==0&&p==0&&q==0){
+	   printf("%d is a superprime",n);	
 	}
 	return 0;
 }
